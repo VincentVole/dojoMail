3bb22ebe65e3cf1b675ff505f85dc00c7cd994d9
Vincent Vole <volevincent@gmail.com>
2017-08-08 17:20:16 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  emails =[
+  	{
+  		email: 'Bill@Gates.com',
+  		importance: true,
+  		subject: 'New Windows',
+  		content: 'Windows XI will launch in year 2100'
+  	},
+  	{
+  		email: 'ada@LoVeLaCe.com',
+  		importance: true,
+  		subject: 'Programming',
+  		content: 'Enchantress of Numbers'
+  	},
+  	{
+  		email: 'john@carmac.COM',
+  		importance: false,
+  		subject: 'updated algo',
+  		content: 'new algo for shadow volumes'
+  	},
+  	{
+  		email: 'gabe@newel.com',
+  		importance: false,
+  		subject: 'hl3!',
+  		content: 'jkjk'
+  	}
+  ]
 }
